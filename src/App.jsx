458495796b571a0b213db025a2b884b594fe370a
luifercat <luifercat@gmail.com>
2023-09-07 19:53:58 -0500

    second commit App.jsx change netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
-import Work from "./components/work";
+import Work from "./components/Work";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
